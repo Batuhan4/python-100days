d50e431a49c0aaab2488d53d03c830b5c71623a7
batuhan4 <batuhanbayazitt@gmail.com>
2022-07-10 23:09:45 +0300

    day1

---
+++ day1/main.py
@@ -0,0 +1,8 @@
+# kolaydan zora bu ilk günler basit sorry    isterseniz direk day 40 a falan atlayın
+print("müzik grubu ismi bulmaca")
+print(" ")
+input("devam etmek için entera basın:  ")
+print(" ")
+city = input("büyüdüğünüz şehir:  ")
+evcil_hayvan = input("evcil hayvanınızım ismi:  ")
+print("aradığınız isim" + " " + city + "  " + evcil_hayvan + " " + "mıydı?")
